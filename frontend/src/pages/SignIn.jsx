50fe4a6ced18649f2df6b4374b417ed74a3c7d20
Salman <khansalman67313@gmail.com>
2026-04-13 17:25:43 +0530

    commit 1

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -3,7 +3,7 @@ import bg from "../assets/authBg.png";
 import { IoEye } from "react-icons/io5";
 import { IoEyeOff } from "react-icons/io5";
 import { Link, useNavigate } from "react-router-dom";
-import { UserDataContext } from "../context/usercontext";
+import { UserDataContext } from "../context/usercontext.jsx";
 import axios from "axios";
 import { useContext } from "react";
 
