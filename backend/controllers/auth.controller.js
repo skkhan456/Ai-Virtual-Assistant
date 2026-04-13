d69d48eb910471fe4918cce221e08027144fbeac
Salman <khansalman67313@gmail.com>
2026-04-13 17:33:32 +0530

    commit 1

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -30,8 +30,8 @@ export const signUp = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      sameSite: "strict",
-      secure: false,
+      sameSite: "none",
+      secure: true,
     });
     console.log(req.body);
     res.status(201).json(newUser);
@@ -58,8 +58,8 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      sameSite: "strict",
-      secure: false,
+      sameSite: "none",
+      secure: true,
     });
     res.status(201).json(user);
   } catch (error) {
